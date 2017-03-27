738f2f00b1f8a99f1fa0043b74737bde542f5f7d
morelook <gezhigang@morelook.cn>
2017-03-27 12:53:50 +0800

    update

--- demo/src/test/java/guda/mvcx/demo/test/TestEventServerStart.java
+++ demo/src/test/java/guda/mvcx/demo/test/TestEventServerStart.java
@@ -1,36 +1,24 @@
-package guda.mvcx.demo;
+package guda.mvcx.demo.test;
 
 import guda.mvcx.core.eventbus.EventBusVerticle;
 import guda.mvcx.core.eventbus.context.AppContext;
 import guda.mvcx.core.eventbus.msg.HttpEventMsg;
 import guda.mvcx.core.eventbus.msg.HttpMsgConvert;
 import guda.mvcx.core.util.JsonConfigUtil;
+import guda.mvcx.demo.ServerStart;
 import io.vertx.core.DeploymentOptions;
 import io.vertx.core.Vertx;
-import io.vertx.core.impl.Args;
 import io.vertx.core.impl.VertxFactoryImpl;
 import io.vertx.core.json.JsonObject;
 import io.vertx.core.spi.VertxFactory;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
- * Created by well on 2017/3/25.
+ * Created by well on 2017/3/27.
  */
-public class EventServerStart {
+public class TestEventServerStart {
 
-    private static Logger log= LoggerFactory.getLogger(EventServerStart.class);
-
-    public static void main(String[] sargs){
-        Args args = new Args(sargs);
-        String confArg = args.map.get("-conf");
-        if(confArg==null){
-            confArg="dev";
-        }
-
-        log.warn("server start use conf:"+confArg);
-
-        JsonObject config= JsonConfigUtil.getConfig(ServerStart.class).getJsonObject(confArg);
+    public static void main(String[] args){
+        JsonObject config= JsonConfigUtil.getConfig(ServerStart.class).getJsonObject("dev");
         JsonObject sys = config.getJsonObject("sys");
         sys.forEach(entry->{
             System.getProperties().put(entry.getKey(),entry.getValue());
@@ -56,7 +44,8 @@ public class EventServerStart {
         });
 
 
-        vertx.deployVerticle("guda.mvcx.core.eventbus.HttpConsumerVerticle",readConsumeOpts(),res -> {
+        int instanceCount=config.getInteger("instance.biz.count",3);
+        vertx.deployVerticle("guda.mvcx.core.eventbus.HttpConsumerVerticle",readConsumeOpts(instanceCount),res -> {
             if (res.succeeded()) {
                 System.out.println("Deployment id is: " + res.result());
             } else {
@@ -73,10 +62,10 @@ public class EventServerStart {
 
 
 
-    public static DeploymentOptions readConsumeOpts(){
+
+    public static DeploymentOptions readConsumeOpts(int instanceCount){
         final DeploymentOptions options = new DeploymentOptions();
-        //options.setHa(false);
-        options.setInstances(5);
+        options.setInstances(instanceCount);
         options.setWorker(true);
         options.setMultiThreaded(true);
         return options;
